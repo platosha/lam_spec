4e47ccc69fcea79870a0653a9a1b4feb42f98d7a
macovsky <robotector@gmail.com>
2011-12-07 14:59:15 +0300

    migrate on all roles

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,6 +9,7 @@ ssh_options[:port]     =  58022
 
 role :app0, "188.127.242.241", :primary => true
 role :app1, "188.127.242.242"
+roles_options = {:roles => [:app0, :app1]}
 
 namespace :deploy do  
   task :default do
@@ -22,22 +23,22 @@ namespace :deploy do
     restart
   end
 
-  task :update, :roles => [:app0, :app1] do
+  task :update, roles_options do
     run "cd #{deploy_to}; git checkout db/schema.rb; git pull; bundle install --deployment"
   end
   
-  task :migrate do
+  task :migrate, roles_options do
     update
     run "cd #{deploy_to} && RAILS_ENV=production bundle exec rake db:migrate"
     precompile_assets
     restart
   end
   
-  task :precompile_assets, :roles => [:app0, :app1] do
+  task :precompile_assets, roles_options do
     run "cd #{deploy_to}; bundle exec rake assets:precompile"
   end
   
-  task :restart, :roles => [:app0, :app1] do
+  task :restart, roles_options do
     run "cd #{deploy_to}; mkdir -p tmp && touch tmp/restart.txt;"
   end
 end
